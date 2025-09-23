853609afe10ddc0cfc9635448f00aaee8851bc8d
Axl-Lvy <axl@activeviam.com>
2025-09-23 13:54:51 +0200

    cleaning

--- composeApp/src/commonMain/kotlin/proj/tarotmeter/axl/data/model/Identifiable.kt
+++ composeApp/src/commonMain/kotlin/proj/tarotmeter/axl/data/model/Identifiable.kt
@@ -2,7 +2,7 @@ package proj.tarotmeter.axl.data.model
 
 import kotlin.uuid.Uuid
 
-/** Interface for classes with auto-incremented id */
+/** Interface for classes with an id */
 interface Identifiable {
 
   /** Unique id */
