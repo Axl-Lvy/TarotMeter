4f88538467c6d6c5a05d6f58e3e246b6fb6fd9ee
Axl <axeldecesson@live.fr>
2025-11-02 18:33:43 +0100

    format

--- composeApp/src/commonMain/kotlin/proj/tarotmeter/axl/ui/NewGameScreen.kt
+++ composeApp/src/commonMain/kotlin/proj/tarotmeter/axl/ui/NewGameScreen.kt
@@ -1,7 +1,6 @@
 package proj.tarotmeter.axl.ui
 
 import androidx.compose.foundation.BorderStroke
-
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
