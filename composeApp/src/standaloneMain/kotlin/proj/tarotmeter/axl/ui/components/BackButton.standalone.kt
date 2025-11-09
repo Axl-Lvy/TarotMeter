b6104c8b9328d032670f8a294c5462cbbc65b46c
Axel Levy <81815421+Axl-Lvy@users.noreply.github.com>
2025-11-09 12:03:44 +0100

    feat: Website home button (#52)

---
+++ composeApp/src/standaloneMain/kotlin/proj/tarotmeter/axl/ui/components/BackButton.standalone.kt
@@ -0,0 +1,9 @@
+package proj.tarotmeter.axl.ui.components
+
+import androidx.compose.runtime.Composable
+
+/** Noop back button for the standalone application's home screen. */
+@Composable
+actual fun HomeBackButton() {
+  /* No back button on the standalone application's home screen */
+}
