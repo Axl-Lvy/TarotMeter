4871a98623b1d06a1d2bff6bdfde7ba3cff974d2
Axel Levy <81815421+Axl-Lvy@users.noreply.github.com>
2025-11-09 23:54:06 +0100

    fix: Initial route (#54)

--- composeApp/src/jsMain/kotlin/proj/tarotmeter/axl/main.kt
+++ composeApp/src/jsMain/kotlin/proj/tarotmeter/axl/main.kt
@@ -6,11 +6,17 @@ import androidx.navigation.ExperimentalBrowserHistoryApi
 import androidx.navigation.bindToBrowserNavigation
 import kotlinx.browser.document
 import org.jetbrains.skiko.wasm.onWasmReady
+import proj.tarotmeter.axl.util.getInitialRouteFromUrl
 
 @OptIn(ExperimentalComposeUiApi::class, ExperimentalBrowserHistoryApi::class)
 fun main() {
   onWasmReady {
     val body = document.body ?: return@onWasmReady
-    ComposeViewport(body) { App { it.bindToBrowserNavigation() } }
+    ComposeViewport(body) {
+      App(
+        initialRoute = getInitialRouteFromUrl(),
+        onNavHostReady = { it.bindToBrowserNavigation() },
+      )
+    }
   }
 }
--- composeApp/src/wasmJsMain/kotlin/proj/tarotmeter/axl/main.kt
+++ composeApp/src/wasmJsMain/kotlin/proj/tarotmeter/axl/main.kt
@@ -5,9 +5,12 @@ import androidx.compose.ui.window.ComposeViewport
 import androidx.navigation.ExperimentalBrowserHistoryApi
 import androidx.navigation.bindToBrowserNavigation
 import kotlinx.browser.document
+import proj.tarotmeter.axl.util.getInitialRouteFromUrl
 
 @OptIn(ExperimentalComposeUiApi::class, ExperimentalBrowserHistoryApi::class)
 fun main() {
   val body = document.body ?: return
-  ComposeViewport(body) { App { it.bindToBrowserNavigation() } }
+  ComposeViewport(body) {
+    App(initialRoute = getInitialRouteFromUrl(), onNavHostReady = { it.bindToBrowserNavigation() })
+  }
 }
