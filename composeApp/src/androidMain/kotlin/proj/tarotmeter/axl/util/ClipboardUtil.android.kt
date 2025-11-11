4bf847877991b559cd2541efa41ae28be0f4fd8d
Axl-Lvy <axl@activeviam.com>
2025-11-12 00:22:32 +0100

    kdoc

--- composeApp/src/androidMain/kotlin/proj/tarotmeter/axl/util/ClipboardUtil.android.kt
+++ composeApp/src/androidMain/kotlin/proj/tarotmeter/axl/util/ClipboardUtil.android.kt
@@ -3,4 +3,5 @@ package proj.tarotmeter.axl.util
 import android.content.ClipData
 import androidx.compose.ui.platform.toClipEntry
 
+/** Converts a String to a ClipEntry for clipboard operations on Android. */
 actual fun String.toClipEntry() = ClipData.newPlainText(this, this).toClipEntry()
